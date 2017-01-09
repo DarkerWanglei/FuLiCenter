c69d3a29d1d9b794bd6acc4abdf4ce54f23867af
wanglei <912436316@qq.com>
2017-01-09 18:01:30 +0800

    second commit

---
+++ app/src/main/java/cn/ucai/fulicenter/bean/CartgoryGroupBean.java
@@ -0,0 +1,56 @@
+package cn.ucai.fulicenter.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2017/1/9.
+ */
+
+public class CartgoryGroupBean implements Serializable{
+
+    /**
+     * id : 344
+     * name : 最IN
+     * imageUrl : muying/2.jpg
+     */
+
+    private int id;
+    private String name;
+    private String imageUrl;
+
+    public CartgoryGroupBean() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "CartgoryGroupBean{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/cn/ucai/fulicenter/bean/MessageBean.java
@@ -0,0 +1,45 @@
+package cn.ucai.fulicenter.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2017/1/9.
+ */
+
+public class MessageBean implements Serializable{
+
+    /**
+     * success : true
+     * msg : 添加收藏成功
+     */
+
+    private boolean success;
+    private String msg;
+
+    public MessageBean() {
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "MessageBean{" +
+                "success=" + success +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+}
