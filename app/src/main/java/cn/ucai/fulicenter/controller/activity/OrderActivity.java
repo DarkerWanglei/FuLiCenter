e212acedfc2528d2bd06fba11740cf6c96adfda1
wanglei <912436316@qq.com>
2017-02-06 15:15:51 +0800

    模拟付款

--- app/src/main/java/cn/ucai/fulicenter/controller/activity/OrderActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/OrderActivity.java
@@ -1,20 +1,168 @@
 package cn.ucai.fulicenter.controller.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.pingplusplus.android.PingppLog;
+import com.pingplusplus.libone.PaymentHandler;
+import com.pingplusplus.libone.PingppOne;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.I;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements
+        PaymentHandler {
+
+    @BindView(R.id.ivSettingBack)
+    ImageView ivSettingBack;
+    @BindView(R.id.etReceiver)
+    EditText etReceiver;
+    @BindView(R.id.etPhoneNumber)
+    EditText etPhoneNumber;
+    @BindView(R.id.etStreet)
+    EditText etStreet;
+    @BindView(R.id.tv_cart_sum_price)
+    TextView tvCartSumPrice;
+    @BindView(R.id.SpinOrderProvince)
+    Spinner mSpinOrderProvince;
+    int payPrice;
+    private static String URL = "http://218.244.151.190/demo/charge";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        ButterKnife.bind(this);
+        payPrice = getIntent().getIntExtra(I.Cart.PAY_PRICE, 0);
+        initPingPP();
         setView();
     }
 
-    private void setView() {
+    private void initPingPP() {
+        // 设置要使用的支付方式
+        PingppOne.enableChannels(new String[]{"wx", "alipay", "upacp", "bfb", "jdpay_wap"});
+        //提交数据的格式，默认格式为json
+        //PingppOne.CONTENT_TYPE = "application/x-www-form-urlencoded";
+        PingppOne.CONTENT_TYPE = "application/json";
+        //是否开启日志
+        PingppLog.DEBUG = true;
+    }
 
+    private void setView() {
+        tvCartSumPrice.setText("合计:  ￥" + payPrice);
+    }
+
+    @OnClick(R.id.tv_cart_buy)
+    public void checkOrder() {
+        String receiverName = etReceiver.getText().toString();
+        if (TextUtils.isEmpty(receiverName)) {
+            etReceiver.setError("收货人姓名不能为空");
+            etReceiver.requestFocus();
+            return;
+        }
+        String mobile = etPhoneNumber.getText().toString();
+        if (TextUtils.isEmpty(mobile)) {
+            etPhoneNumber.setError("手机号码不能为空");
+            etPhoneNumber.requestFocus();
+            return;
+        }
+        if (!mobile.matches("[\\d]{11}")) {
+            etPhoneNumber.setError("手机号码格式错误");
+            etPhoneNumber.requestFocus();
+            return;
+        }
+        String area = mSpinOrderProvince.getSelectedItem().toString();
+        if (TextUtils.isEmpty(area)) {
+            Toast.makeText(this, "收货地区不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String address = etStreet.getText().toString();
+        if (TextUtils.isEmpty(address)) {
+            etStreet.setError("街道地址不能为空");
+            etStreet.requestFocus();
+            return;
+        }
+        showPay();
+    }
+
+    private void showPay() {
+        // 产生个订单号
+        String orderNo = new SimpleDateFormat("yyyyMMddhhmmss")
+                .format(new Date());
+
+        // 计算总金额（以分为单位）
+        int amount = payPrice * 100;
+        JSONArray billList = new JSONArray();
+//        for (Good good : mList) {
+//            amount += good.getPrice() * good.getCount() * 100;
+//            billList.put(good.getName() + " x " + good.getCount());
+//        }
+        // 构建账单json对象
+        JSONObject bill = new JSONObject();
+
+        // 自定义的额外信息 选填
+        JSONObject extras = new JSONObject();
+        try {
+            extras.put("extra1", "extra1");
+            extras.put("extra2", "extra2");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            bill.put("order_no", orderNo);
+            bill.put("amount", amount);
+            bill.put("extras", extras);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //壹收款: 创建支付通道
+        PingppOne.showPaymentChannels(getSupportFragmentManager(), bill.toString(), null, URL, new PaymentHandler() {
+
+            // 返回支付结果
+            // @param data
+
+            @Override
+            public void handlePaymentResult(Intent data) {
+            }
+        });
+    }
+
+    @Override
+    public void handlePaymentResult(Intent data) {
+        if (data != null) {
+            /**
+             * code：支付结果码  -2:服务端错误、 -1：失败、 0：取消、1：成功
+             * error_msg：支付结果信息
+             */
+            int code = data.getExtras().getInt("code");
+            String errorMsg = data.getExtras().getString("error_msg");
+            Log.i("main", "code=" + code);
+        }
+    }
+
+    @OnClick(R.id.ivSettingBack)
+    public void onClick() {
+        finish();
     }
 }
